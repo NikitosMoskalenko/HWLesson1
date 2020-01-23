29ddf9d0ed27bdf0631fde7efe4772e302415d22
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-01-23 11:13:10 +0300

    new commit

--- AlertControllerHomeWork/ViewController.swift
+++ AlertControllerHomeWork/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//      MARK: - Public methods
+
+    func alert(title: String, message: String, style: UIAlertController.Style, text: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        
+        let action = UIAlertAction.init(title: "OK", style: .cancel) { (action) in
+            if let alertControlletText = alertController.textFields?.first?.text{
+                if alertControlletText == text {
+                    let answerAlertController = UIAlertController(title: "Congratulation!", message: "You are right!", preferredStyle: .actionSheet)
+                    answerAlertController.addAction(action)
+                    self.present(answerAlertController, animated: true, completion: nil)
+                } else {
+                    let loseAnswer = UIAlertController(title: "False!", message: "You are wrong!", preferredStyle: .actionSheet)
+                    loseAnswer.addAction(action)
+                    self.present(loseAnswer, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        alertController.addTextField(configurationHandler: nil)
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
     }
+    
+//    MARK: - Actions
 
-
+      @IBAction func firstButton(_ sender: Any) {
+        self.alert(title: "2 + 2", message: "= ?", style: .alert, text: "4")
+      }
+      
+      @IBAction func SecondButton(_ sender: Any) {
+        self.alert(title: "2 + 2 * 2", message: "= ?", style: .alert, text: "6")
+      }
 }
 
